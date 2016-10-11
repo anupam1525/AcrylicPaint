5df1af910506d78b4df4bf7da4e19aeeccbd307f
James Dearing <dearingj@lifetime.oregonstate.edu>
2016-10-10 17:45:36 -0700

    Adding my name to the copyright header

--- app/src/main/java/anupam/acrylic/EasyPaint.java
+++ app/src/main/java/anupam/acrylic/EasyPaint.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014, 2016 Valerio Bozzolan
+ * Copyright (C) 2014, 2016 Valerio Bozzolan & James Dearing
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
